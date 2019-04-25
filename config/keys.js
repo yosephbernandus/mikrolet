fe0bb166e793317f84cf00d9849ee06801b44571
yosephbe1509 <yosephbernandus@gmail.com>
2019-04-25 17:19:51 +0800

    First Commit Workspace Area

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://alva:alva2018@ds147436.mlab.com:47436/mikro_online",
+  secretOrKey: "secret"
+};
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+// const profile = require("./routes/api/profile");
+// const posts = require("./routes/api/posts");
+
+const app = express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Mongo Connect Success"))
+  .catch(err => console.log(err));
+
+// passport middleware
+app.use(passport.initialize());
+
+// passport config
+require("./config/passport")(passport);
+
+// use routes
+app.use("/api/users", users);
+// app.use("/api/profile", profile);
+// app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
